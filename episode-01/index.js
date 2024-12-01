e6c7dc878f10195d61985b90a13f7413ae17b67d
Arif Khambali <arrrf121@gmail.com>
2024-12-02 06:12:53 +0700

    update

--- episode-01/index.js
+++ episode-01/index.js
@@ -1,1 +1,17 @@
-console.log("hallo dunia")
+// console.log("hallo dunia") // use "", '' / s``
+// console.log('i like pizza')
+
+// window.alert(`this alert`)
+// window.alert(`i like [pizza]`)
+
+//comment
+
+/*
+coment multi 
+lines
+many many line
+*/
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = "i like pizza";
+
