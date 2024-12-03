db674c56b4727f9dc028142d6fea58e460c421c2
Arif Khambali <arrrf121@gmail.com>
2024-12-03 18:50:02 +0700

    update learn variable, string, boolean

--- episode-02/index.js
+++ episode-02/index.js
@@ -1,28 +1,65 @@
-// varible = container that store a value
-//          Behaves as if it were the value it contains
+// // varible = container that store a value
+// //          Behaves as if it were the value it contains
 
-// 1. declaration = let x;
-// 2. assigmnet  x =100;
+// // 1. declaration = let x;
+// // 2. assigmnet  x =100;
 
-// 1.
-let x;
-x = 100;
 
-console.log(x);
+// // start varible
+// // 1.
+// let x;
+// x = 100;
 
-//2. 
-let y = 120;
+// console.log(x);
 
-console.log(y);
+// //2. Start Number
+// let y = 120;
 
+// console.log(y);
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// console.log(age);
+// console.log(price);
+// console.log(gpa);
+
+// console.log(`You are ${age} years old`);
+// console.log(`the price is ${price}`);
+// console.log(`your gpa ${gpa}`);
+// console.log(typeof gpa); 
+
+// //end number
+
+// //3. Start Strings
+
+// let firstName = "Bro";
+// let favoriteFood = "pizza";
+// let email = "brocode@gmail.com"
+
+// console.log(typeof firstName); 
+// console.log(`Your name is ${firstName}`);
+// console.log(`you like ${favoriteFood}`);
+// console.log(`your email is ${email}`)
+
+// //end string
+
+// //start boolean
+// let bagian3 = "Boolean";
+// let online = false;
+// let forSale = true;
+
+
+// console.log(`belajar bagian 3 ${bagian3}`);
+// console.log(`Bro is online : ${online}`);
+// console.log(`The item for sale : ${forSale}`);
+// // end boolean
+
+let fullName = "Bro";
 let age = 25;
-let price = 10.99;
-let gpa = 2.1;
+let isStudent = false;
 
-console.log(age);
-console.log(price);
-console.log(gpa);
-
-console.log(`You are ${age} years old`);
-console.log(`the price is ${price}`);
-console.log(`your gpa ${gpa}`);
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your are ${age} years old`;
+document.getElementById("p3").textContent = `Are You Students ? ${isStudent}`;
