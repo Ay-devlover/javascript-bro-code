7084414c915462556d731927a3c171e2adeba1be
Arif Khambali <arrrf121@gmail.com>
2024-12-03 07:33:53 +0700

    episode 02

---
+++ episode-02/index.js
@@ -0,0 +1,28 @@
+// varible = container that store a value
+//          Behaves as if it were the value it contains
+
+// 1. declaration = let x;
+// 2. assigmnet  x =100;
+
+// 1.
+let x;
+x = 100;
+
+console.log(x);
+
+//2. 
+let y = 120;
+
+console.log(y);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(age);
+console.log(price);
+console.log(gpa);
+
+console.log(`You are ${age} years old`);
+console.log(`the price is ${price}`);
+console.log(`your gpa ${gpa}`);
