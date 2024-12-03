f8a8a21281990cb22fc7e9c370fbf156b05ab840
Arif Khambali <arrrf121@gmail.com>
2024-12-04 06:06:43 +0700

    episode-03

---
+++ episode-03/index.js
@@ -0,0 +1,46 @@
+// aritmatic operatos = operand (value, variebale, ets)
+//                      operator (+ - * /)
+//                      ex. 11  =+ * - / 3
+
+let students = 30;
+
+//start operator
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2; 
+// students = students ** 2; //exponen
+// students = students % 3; 
+
+// end operator
+// start augmented assigment
+// students += 3;
+// students -= 1;
+// students *= 3;
+// students /= 3;
+// students **= 3;
+// students %= 2;
+// end augmented assigment
+
+
+// start increment, decrement
+// students++;
+// students--;
+// end increment, decrement
+
+// console.log(students);
+
+// start precedence
+/*
+operator precedence
+1. parenthesis ()
+2. exponents
+3. multiapliction & division & modulo
+4. addition & subtraction
+*/
+// let result = 1 + 2 * 3 + 4 ** 2;  //23
+// let result = 12 % 5 + 8 / 2; //6
+let result = 6 / 2 ** (2 + 5); //0.046875
+// end precedence
+
+console.log(result);
