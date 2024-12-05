1d1e2bb2586f01e9b7fa5082b2e2cff05e5f3f2c
Arif Khambali <arrrf121@gmail.com>
2024-12-06 06:29:41 +0700

    Episode-05

---
+++ episode-05/index.js
@@ -0,0 +1,35 @@
+// type conversion = change the datatype of a value to another
+//                                 (strings, numbers, booleans)
+
+//example 1
+
+// let age = window.prompt("how old are you?");
+// // age = Number(age);
+// age+=1;
+
+// console.log(age, typeof age);
+
+// example 2
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+// let x = "0";
+// let y = "0";
+// let z = "0";
+
+// let x = "";
+// let y = "";
+// let z = "";
+
+let x;
+let y;
+let z;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
